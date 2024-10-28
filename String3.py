34bea36842334a0e73f01e3b19a4a35bc892a968
Shabhaizafar <zafarshabhai03@gmail.com>
2024-10-28 12:03:35 +0530

    String3

---
+++ String3.py
@@ -0,0 +1,34 @@
+# mystr = "Royal"
+# print(mystr)
+
+# center 
+# print("|",mystr.center(30),"|",sep="")  # value > mystr length
+
+# encode 
+# print(mystr.encode(encoding="UTF-8"))
+# A : 65 : 
+
+# Assaymblly lang :  sd4
+# Machine Lang : 0/1  
+
+# zafar
+
+# zafar 
+# werty78 = zafar
+
+# format  : 
+# mystr1 = "Lazy"
+# mystr2 = "Dhruv"
+
+# print(mystr1)
+# print("Dhruv is a Clever boy.")
+
+
+# String FOrmatting : 
+# print(f"{mystr1} is a {mystr2}  boy.")
+
+# print("{} is a {} boy.".format(mystr1,mystr2))
+#       0       1                 0      1
+#       1
+
+
