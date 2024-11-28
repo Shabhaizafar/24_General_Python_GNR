cb4458b2aa4d913c22573f79c8aa7c752c8da49d
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-28 12:01:21 +0530

    Set2

---
+++ Set/SetExample.py
@@ -0,0 +1,13 @@
+# Set
+A = {2, 4, 5, 6}
+# List
+# Compared Value's only
+lis = [1, 2, 3, 4, 5]
+# Dictionary dict, Set is formed on Keys
+# dict = {1: 'Apple', 2: 'Orange'}
+
+# Dictionary dict2
+dict2 = {'Apple': 1, 'Orange': 2}
+# print("Set A and List lis disjoint?", A.isdisjoint(lis))
+# print("Set A and dict are disjoint?", A.isdisjoint(dict))
+print("Set A and dict2 are disjoint?", A.isdisjoint(dict2))
---
+++ Set/set2.py
@@ -0,0 +1,92 @@
+# mySet = {True,False,11,1001}
+
+# print(mySet)
+# # mySet.pop()
+# mySet.remove(True)
+# print(mySet)
+# dict{}
+# tuple ()
+# list []
+# set {}
+
+# mySet1 = set([11,12,13,14,15])
+# mySet2 = set([11,1,13,14,5])
+
+# print(mySet1)
+# print(mySet2)
+
+# mySet1.difference
+# return new Set   : new variable stored
+#  not effect in orignal  Sets
+# mySet2.difference(mySet1)
+# print(mySet2.difference(mySet1)) # {1,5}
+# print(mySet1.difference(mySet2)) # {12,15}
+
+
+
+# mySet1.difference_update
+# return None
+# Modifed Org Set 
+# mySet1.difference_update(mySet2)
+# print(mySet1)
+# print(mySet2)
+
+#################################################
+# mySet1 = set([11,12,13,14,15])
+# mySet2 = set([11,1,13,14,5])
+
+# print(mySet1)
+# print(mySet2)
+# mySet1.intersection
+# return new Set   : new variable stored
+#  not effect in orignal  Sets
+
+# Same
+# print(mySet1.intersection(mySet2))
+# print(mySet2.intersection(mySet1))
+
+
+
+# mySet1.intersection_update
+# mySet1 = set([11,12,13,14,15])
+# mySet2 = set([11,1,13,14,5])
+
+# mySet2.intersection_update(mySet1)
+# print(mySet1)
+# print(mySet2)
+
+#################################################
+# mySet1 = set([11,12,13,14,15])
+# mySet2 = set([11,1,13,14,5])
+
+# # print(mySet1)
+# # print(mySet2)
+# mySet1.symmetric_difference 
+# print(mySet1.symmetric_difference(mySet2))
+
+# mySet1.symmetric_difference_update
+# mySet1.symmetric_difference_update(mySet2)
+# mySet2.symmetric_difference_update(mySet1)
+# mySet2.symmetric_difference_update(mySet2)
+
+
+# print(mySet1)
+# print(mySet2)
+################################
+# mySet1 = set([1,12,13,14,15])
+# mySet2 = set(["True"])
+
+# print(mySet1)
+# print(mySet2)
+# # mySet1.isdisjoint
+# print(mySet1.isdisjoint(mySet2))
+
+
+
+# mySet1.issubset
+
+
+
+
+
+# mySet1.discard
