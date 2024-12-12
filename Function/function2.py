039457ee583977eb73168ad8fcb0a441e0b1f6ce
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-12 11:37:05 +0530

    Lamda Task

--- Function/function2.py
+++ Function/function2.py
@@ -32,9 +32,9 @@
 # Quadruple the number of 15 = 60
 # Quintuple the number 15 = 75
 
-def myFu(i):
-    ans = lambda num : num*i 
-    print(ans(int(input("Enter the Value of N : "))))
+# def myFu(i):
+#     ans = lambda num : num*i 
+#     print(ans(int(input("Enter the Value of N : "))))
 
 # myFu(2)
 # myFu(5)
@@ -48,12 +48,22 @@ def myFu(i):
 # [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
 
 
+# myList  = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+    #           0         1
+#                   0               1                  2                    3
+
+# print(sorted(myList,key= lambda a:a[1]))
+
+# print(myList[1][0])
+    
+
 # 4. Write a Python program to sort a list of dictionaries using Lambda.
 # Original list of dictionaries :
 # [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
 # Sorting the List of dictionaries :
 # [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}]
 
+#----------------------------------------------------------------
 
 # 5. Write a Python program to filter a list of integers using Lambda.
 # Original list of integers:
@@ -62,3 +72,67 @@ def myFu(i):
 # [2, 4, 6, 8, 10]
 # Odd numbers from the said list:
 # [1, 3, 5, 7, 9]
+
+# myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+# print(myList)
+
+# ans1 = filter(lambda x : x%2==0,myList)
+# ans2 = filter(lambda x : x%2!=0,myList)
+# # ans = filter(lambda x : x%3==0 and x%2==0,myList)
+
+# print(list(ans1))
+# print(list(ans2))
+# ------------------------------------------------------------ # 
+
+# 6. Write a Python program to square and cube every number in a given list of integers using Lambda.
+# Original list of integers:
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Square every number of the said list:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# Cube every number of the said list:
+# [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+
+# myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+# print(myList)
+
+# ans1 = map(lambda x : pow(x,2),myList)
+# ans2 = map(lambda x : x**3,myList)
+
+# print(list(ans1))
+# print(list(ans2))
+# ------------------------------------------------------------ #
+# 7. Write a Python program to find if a given string starts with a given character using Lambda.
+# Sample Output:
+# True
+# False
+
+# mystr = "Royal Technosoft pvt ltd"
+# print(mystr.startswith('r'))
+# ans = lambda x,ch : x.startswith(ch)
+# print(ans(mystr,'r'))
+
+
+
+# 9. Write a Python program to check whether a given string is a number or not using Lambda.
+# Sample Output:
+# True
+# True
+# False
+# True
+# False
+# True
+
+# Print checking numbers:
+# True
+# True
+
+# 10. Write a Python program to create Fibonacci series up to n using Lambda.
+# Fibonacci series upto 2:
+# [0, 1]
+# Fibonacci series upto 5:
+# [0, 1, 1, 2, 3]
+# Fibonacci series upto 6:
+# [0, 1, 1, 2, 3, 5]
+# Fibonacci series upto 9:
+# [0, 1, 1, 2, 3, 5, 8, 13, 21]
