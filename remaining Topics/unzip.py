f84b966caebd2bbba233da940b505765b169db0b
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-26 11:45:31 +0530

    zip,unzip

---
+++ remaining Topics/unzip.py
@@ -0,0 +1,28 @@
+# myList1 = [11,12,13]
+# myList2 = [1,2,3]
+
+# print("List 1 : ",myList1)
+# print("List 2 : ",myList2)
+
+# print("Zipping : ")
+# zipData = zip(myList1,myList2)
+# print("Data Format : ",zipData)
+# print("Zip Data : ",tuple(zipData))
+
+
+# myTuple  = tuple(zip(myList1,myList2))
+# print(myTuple)
+
+
+# print("UnZip : ")
+# unzip = zip(*myTuple)
+
+# print("Data Unzip Object :",unzip)
+# print("UnZip Data : ",tuple(unzip))
+
+
+
+# /////////////////////////
+# Unzipping Data into Separate Lists : 
+
+# You are given a list of tuples where each tuple contains a name and age (e.g., ("Alice", 25)). How can you unzip this data into two separate lists: one for names and another for ages?
