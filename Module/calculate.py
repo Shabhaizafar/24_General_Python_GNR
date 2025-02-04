8f37814fe29ecff001070141dc76d80622cb6031
Shabhaizafar <zafarshabhai03@gmail.com>
2025-02-04 09:10:02 +0530

    module continue

---
+++ Module/calculate.py
@@ -0,0 +1,20 @@
+def addition(n1,n2):
+    return n1+n2 
+
+def subtraction(n1,n2):
+    return n1-n2
+
+def multiplication(n1,n2):
+    return n1*n2
+
+
+def print_user_data(fname,lname):
+    print(f'Your Name is {fname} {lname}')
+
+
+class module :
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+    
+
---
+++ Module/code3.py
@@ -0,0 +1,158 @@
+# import calculate
+
+# a = 12
+# b = 10
+
+# print(calculate.addition(a,b))
+
+# print(calculate.subtraction(a,b))
+
+
+# from calculate import addition,multiplication
+
+# a = 12
+# b = 10
+
+# print(addition(a,b))
+
+# print(multiplication(a,b))
+
+
+
+
+# from calculate import print_user_data as biodata
+
+# fname=  "Raj"
+# lname = "Shah"
+# # print_user_data(fname,lname)
+
+# biodata(fname,lname)
+
+# from calculate import module 
+
+# obj1 = module("Zafar","Shabhai")
+
+# print(obj1.firstname)
+# print(obj1.lastname)
+
+
+# from calculate import module as details
+
+# obj1 = details("Zafar","Shabhai")
+
+# print(obj1.firstname)
+# print(obj1.lastname)
+
+# from calculate import module as details ,addition as add
+# from calculate import module as details
+
+# obj1 = details("Zafar","Shabhai")
+
+# print(obj1.firstname)
+# print(obj1.lastname)
+
+# print(add(12,13))
+
+
+
+
+
+
+############################################################
+# Module - random
+
+# 1. Write a Python program to generate a random color hex, a random alphabetical string, random value between two integers (inclusive) and a random multiple of 7 between 0 and 70. Use random.randint()
+# Click me to see the sample solution
+
+# 2. Write a Python program to select a random element from a list, set, dictionary-value, and file from a directory. Use random.choice()
+
+# Click me to see the sample solution
+
+# 3. Write a Python program that generates random alphabetical characters, alphabetical strings, and alphabetical strings of a fixed length. Use random.choice()
+
+# Click me to see the sample solution
+
+# 4. Write a Python program to construct a seeded random number generator, also generate a float between 0 and 1, excluding 1. Use random.random()
+
+# Click me to see the sample solution
+
+# 5. Write a Python program to generate a random integer between 0 and 6 - excluding 6, random integer between 5 and 10 - excluding 10, random integer between 0 and 10, with a step of 3 and random date between two dates. Use random.randrange()
+
+# Click me to see the sample solution
+
+# 6. Write a Python program to shuffle the elements of a given list. Use random.shuffle()
+
+# Click me to see the sample solution
+
+# 7. Write a Python program to generate a float between 0 and 1, inclusive and generate a random float within a specific range. Use random.uniform()
+
+# Click me to see the sample solution
+
+# 8. Write a Python program to create a list of random integers and randomly select multiple items from the said list. Use random.sample()
+
+# Click me to see the sample solution
+
+# 9. Write a Python program to set a random seed and get a random number between 0 and 1. Use random.random.
+# Click me to see the sample solution
+
+
+##########################################################
+#Module  datetime : 
+
+# 1. Write a Python script to display the various Date Time formats -
+# a) Current date and time
+# b) Current year
+# c) Month of year
+# d) Week number of the year
+# e) Weekday of the week
+# f) Day of year
+# g) Day of the month
+# h) Day of week
+
+# Click me to see the solution
+
+# 2. Write a Python program to determine whether a given year is a leap year.
+
+# Click me to see the solution
+
+# 3. Write a Python program to convert a string to datetime.
+# Sample String : Jul 1 2014 2:43PM
+# Expected Output : 2014-07-01 14:43:00
+
+# Click me to see the solution
+
+# 4. Write a Python program to get the current time in Python.
+# Sample Format :  13:19:49.078205
+
+# Click me to see the solution
+
+# 5. Write a Python program to subtract five days from the current date.
+# Sample Date :
+# Current Date : 2015-06-22
+# 5 days before Current Date : 2015-06-17
+
+# Click me to see the solution
+
+# 6. Write a Python program to convert a Unix timestamp string to a readable date.
+# Sample Unix timestamp string : 1284105682
+# Expected Output : 2010-09-10 13:31:22
+
+# Click me to see the solution
+
+# 7. Write a Python program to print yesterday, today, tomorrow.
+
+# Click me to see the solution
+
+# 8. Write a Python program to convert the date to datetime (midnight of the date) in Python.
+# Sample Output : 2015-06-22 00:00:00
+
+# Click me to see the solution
+
+# 9. Write a Python program to print the next 5 days starting today.
+
+# Click me to see the solution
+
+# 10. Write a Python program to add 5 seconds to the current time.
+# Sample Data :
+# 13:28:32.953088
+# 13:28:37.953088
