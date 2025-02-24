3cc9dab598241dac7537678717e94cb8462df95d
Shabhaizafar <zafarshabhai03@gmail.com>
2025-02-24 08:57:09 +0530

    Exception Handling

---
+++ Try_Catch/try.py
@@ -0,0 +1,125 @@
+# Try : 
+# The try block lets you test a block of code for errors.
+
+# Except : 
+# The except block lets you handle the error.
+
+# Else : 
+# The else block lets you execute code when there is no error.
+
+# Finaly : 
+# The finally block lets you execute code, regardless of the result of the try- and except blocks.
+
+# num1 = 12  
+# num2 = 13
+# print(num1)
+
+# print(num2)
+
+
+# Try 
+# num1=12
+# num2=13
+# try: 
+#     print(num1)
+# except:
+#     print("Error Handle !!")
+
+# print(num2)
+
+
+
+# num1=12
+# num2=13
+# try: 
+#     # print(num1)
+#     10 * (1/0)
+# except NameError:
+#     print("Error Handle !!")
+# except ZeroDivisionError :
+#     print("ZeroDivisionError")
+# # except : 
+# #     print("Another !!")
+
+# print(num2)
+
+
+
+
+
+# try :                           #if                     #for
+#     print(12)   
+# except ZeroDivisionError:               #elif
+#     print("ZeroDivisionError")
+# # default Block !!!!!!  when try Successfully Execute 
+# else :                                  # else          #else
+#     print("Else Block !!!")
+
+
+
+# for i in range(0,10):
+#     print(i)
+#     if(i==5):
+#         j = 5
+#     try :
+#         print(j)
+#     except : 
+#         print("Error")
+#     else: 
+#         print("Try Successfully Execute !!!")
+# else:
+#     print("For Loop End !!!")
+
+
+
+# str="Royal"
+# str2 = "royal"
+# try:
+#     print(str==str2)
+# except NameError: 
+#     print("NameError")
+# else : 
+#     print("Else Block !!")
+# finally :      
+#     print("Finally Block !!")
+
+#######################################################
+# 1. Write a Python program to handle a ZeroDivisionError exception when dividing a number by zero.
+
+#-----------------------------------------
+
+# 2. Write a Python program that prompts the user to input an integer and raises a ValueError exception if the input is not a valid integer.
+
+#-----------------------------------------
+
+# 3. Write a Python program that opens a file and handles a FileNotFoundError exception if the file does not exist.
+
+#-----------------------------------------
+
+# 4. Write a Python program that prompts the user to input two numbers and raises a TypeError exception if the inputs are not numerical.
+
+#-----------------------------------------
+
+# 5. Write a Python program that opens a file and handles a PermissionError exception if there is a permission issue.
+
+#-----------------------------------------
+
+# 6. Write a Python program that executes an operation on a list and handles an IndexError exception if the index is out of range.
+
+#-----------------------------------------
+
+# 7. Write a Python program that prompts the user to input a number and handles a KeyboardInterrupt exception if the user cancels the input.
+
+#-----------------------------------------
+
+# 8. Write a Python program that executes division and handles an ArithmeticError exception if there is an arithmetic error.
+
+#-----------------------------------------
+
+# 9. Write a Python program that opens a file and handles a UnicodeDecodeError exception if there is an encoding issue.
+
+#-----------------------------------------
+
+# 10. Write a Python program that executes a list operation and handles an AttributeError exception if the attribute does not exist.
+
+#-----------------------------------------
